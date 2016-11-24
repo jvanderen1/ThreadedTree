a3661e54ab031e07ffa1e574e3698d8cee4614ef
jvanderen1 <vanderj3@my.erau.edu>
2016-11-24 15:56:35 -0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Threaded_Tree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Threaded_Tree.dir/ThreadedTree.c.o"
+  "Threaded_Tree.pdb"
+  "Threaded_Tree"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Threaded_Tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
